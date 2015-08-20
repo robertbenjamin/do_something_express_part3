450cf98293e8d7bfeecd8ffdbaf38964798ab097
Robert <i.am.robert.benjamin@gmail.com>
2015-08-20 00:00:21 -0400

    Update list model
    I’m assuming I don’t need to manually specify an id anymore.

--- app/models/list.js
+++ app/models/list.js
@@ -1,5 +1,12 @@
-module.exports = [
+/*module.exports = [
   {id: 1, title:"Errands"},
   {id: 2, title:"Things that are better than WDI"},
   {id: 3, title:"WDI To-Dos"}
-]
+]*/
+
+module.exports = function(sequelize, Sequelize) {
+	var model = sequelize.define("list", {
+		title: Sequelize.STRING
+	})
+	return model
+}
