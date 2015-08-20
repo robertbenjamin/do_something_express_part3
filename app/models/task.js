1c9e62041c3776572f001ecbe55aded66355c1c1
Robert <i.am.robert.benjamin@gmail.com>
2015-08-20 00:02:31 -0400

    Add task model

--- app/models/task.js
+++ app/models/task.js
@@ -1,5 +1,15 @@
-module.exports = [
+/*module.exports = [
   {id: 1, body:"Water the plants", completed: false, listId: 1},
   {id: 2, body:"Feed the cat", completed: true, listId: 1},
   {id: 3, body:"Send the WDI instructors a nice card", completed: false, listId: 3}
 ]
+*/
+
+module.exports = function(sequelize, Sequelize) {
+	var model = sequelize.define("task", {
+		body: Sequelize.STRING,
+		completed: Sequelize.BOOLEAN,
+		listId: Sequelize.INTEGER
+	})
+	return model
+}
