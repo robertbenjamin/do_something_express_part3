55ef16eff405bda5dec80bc983f1e9cb616e7e1e
Robert <i.am.robert.benjamin@gmail.com>
2015-08-20 00:34:09 -0400

    Update list controller
    Heavily based on Tunr.

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var List = require("../models/list")
+var DB = require("../../config/connection");
+var List = DB.models.List;
 
 function error(response, message){
   response.status(500);
@@ -8,40 +9,45 @@ function error(response, message){
 }
 
 router.get("/lists", function(req, res){
-  return res.json(List);
+  List.findAll({order: "id"}).then(function(lists){
+    res.json(lists);
+  });
 });
 
 router.post("/lists", function(req, res){
-  List.push(req.body);
-  return res.json(req.body);
+  List.create(req.body)
+  .then(function(list) {
+    res.json(list);
+  })
 });
 
 router.get("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      return res.json(List[l]);
-    }
-  }
+  List.findById(req.params.id)
+  .then(function(list) {
+    res.json(list);
+  });
 });
 
 router.put("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      List[l] = req.body;
-      return res.json(List[l]);
-    }
-  }
-  return error(res, "not found");
+  List.findById(req.params.id)
+  .then(function(list) {
+    if(!list) return error(res, "No list with this id!")
+    return list.updateAttributes(req.body)
+  })
+  .then(function(list) {
+    res.json(list)
+  });
 });
 
 router.delete("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      delete List[l];
-      return res.json(List[l]);
-    }
-  }
-  return error(res, "not found");
+  List.findById(req.params.id)
+  .then(function(list) {
+    if(!list) return error(res, "No list with this id!")
+      return list.destroy()
+  })
+  .then(function() {
+    res.redirect("/lists")
+  })
 });
 
 module.exports = router;
